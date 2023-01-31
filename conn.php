a8c7e26d1e1c07162d40e1e006be0ef052d15ca7
João ielen <99980161+awakeJa1@users.noreply.github.com>
2023-01-31 17:25:17 -0300

    Add files via upload

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php
-$conn=new PDO('mysql:host=localhost;dbname=Be-Ut','root','');
+$conn=new PDO('mysql:host=localhost;dbname=banco do meteus','root','');
 ?>
 <!--ligação com o banco de dados, nome do banco e user e senha do usuario-->

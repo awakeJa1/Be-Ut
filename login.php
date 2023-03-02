4959f10cc19d94ce44e21e43bf3c527934273296
João ielen <99980161+awakeJa1@users.noreply.github.com>
2023-03-02 03:30:52 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -6,9 +6,9 @@
 </head>
 
   <header class="header"> <!--cabeçalho da página, tudo em verde-->
-    <a class="header-name" href="index.php">Be-UT</a>
+    <a class="header-name" rel="next" href="index.php">Be-UT</a>
       <nav class="header-menu">
-          <a class="header-menu-item" href="produtos.php">Produtos</a>
+          <a class="header-menu-item">Promoções</a>
           <a class="header-menu-item">Recentes</a>
           <a class="header-menu-item">Carrinho</a>
           <a class="header-menu-item">Configurações</a>
@@ -20,65 +20,69 @@
 <!-- aqui começa a parte da foto, bio e respostas da pessoa-->
 
 <main class="main">
-    
-<section class="main-section1"> <!--primeira divisão da página-->
-  <h1 class="text-section1";>Informações do perfil</h1>
-    <div>
-      <img class="div-profilepic" src="https://cdn.discordapp.com/attachments/742780153146376213/1070043899239870464/26.png" width="100px" height="100px" alt="imagem">
-    
+  <body> 
+  <section class="main-section1"> <!--primeira divisão da página-->
+    <h1 class="text-section1">Informações do perfil</h1>
+  <div>
+    <img class="div-profilepic" src="https://novacatclinic.com/wp-content/uploads/2013/08/Cat_using_computer.jpg" width="100px" height="100px">
   
-    <input type="text" name="bio" name="Digite aqui sua biografia" class="bio-text"></input> <!-- mudar para o que a pessoa escrever -->
-    <p class="form-text">respostas</p> <!-- mudar para o que a pessoa escrever -->
+    </br></br>Digite aqui sua biografia</br><input type="text" name="bio" name="Digite aqui sua biografia" class="bio-text"></br></input> <!-- mudar para o que a pessoa escrever -->
+    <p class="form-text">respostas</p> <!-- respostas do formulario -->
   
-  </br>
-</div>
-</section>
-
-<section class="main-section2"> <!--segunda divisão da página-->
-<div>
+    </br>
+  
+  </section>
+  <section class="main-section2"> <!--segunda divisão da página-->
   <button class="feed-button" type="button" name="feed">Ir para feed</button> <!--botao que leva para o feed de produtos-->
     </br>
     </br>
   <button class="back-button" type="button" name="voltar">Voltar</button> <!--botao que leva para o index-->
  
-  <p class="info-da-conta">Informações pessoais</p> <!-- nome, email, telefone, etc -->
+</div>
+
+</section>
+
+</body>
+
+<div2> 
+<b class="info-da-conta">Informações pessoais</b> <!-- nome, email, telefone, etc -->
+  <p class="info-text">Nome: Nome</p>
+  <p class="info-text">Email: Email@email.com 1</p>
+  <p class="info-text">Idade: XX</p>
   <p class="info-text">Pergunta 1: Resposta 1</p> <!--respostas do formulário-->
   <p class="info-text">Pergunta 2: Resposta 2</p> <!-- puxar as respostas do formulario -->
   <p class="info-text">Pergunta 3: Resposta 3</p>
   <p class="info-text">Pergunta 4: Resposta 4</p>
- 
-</div>
-</section>
 
+</div2>
 
 </main>
 
-  <footer class="footer"--> <!--rodapé da página-->
+  <!--footer class="footer"--> <!--rodapé da página
      <p class="footer-text">All Rights Reserved©</p>
           <a class="footer-item">Qualquer coisa</a>
           <a class="footer-item">Qualquer coisa</a>
           <a class="footer-item">Qualquer coisa</a>
           <a class="footer-item">Qualquer coisa</a>
-  </footer>
+  </footer-->
 
 <style>
 
 div{ /* estiliza TODAS as divs */
-  background-color: #d5d5d5;
-  width: 707px;
-  height: 650px;
-  
+  height: 500px;
+  background-color: #7678FF;
 }
 body { /* pagina inteira */
   font-size: 100%;
-  background: color #FFFFFF;
+  background-color: #7678FF;
   margin: 0px;
+  align-items: center;
 }
 
 .header { /*parte de cima*/ 
   padding: 18px;
   margin: 0px;
-  background-color: #93cbf5;
+  background-color: #6D3BFF;
   font-family: 'Inconsolata', monospace;
   display: flex;
   flex-direction: row;
@@ -104,7 +108,6 @@ body { /* pagina inteira */
 }
 
 .main { /* estiliza tudo dentro da classe main */ 
-
 }
 
 .main-section1{ /* estiliza tudo dentro da primeira section*/
@@ -112,7 +115,6 @@ body { /* pagina inteira */
   justify-content: space-around;
   align-items: left;
   flex-direction: column;
-
 }
 
 .text-section1{ /* estiliza apenas o texto da primeira section*/
@@ -123,27 +125,29 @@ body { /* pagina inteira */
   margin: 10px;
 }
 
-.div-profilepic{ /*estiliza apenas a imagem dentro da section*/
+.div-profilepic{ /*estiliza apenas a imagem de perfil*/
   border: 3px solid #000000;
-  height: 20px 20px;
+  height: 100px 100px;
 }
 
 .bio-text{
-
 }
 
-.main-section2 { /* estiliza tudo que está dentro da segunda section*/
+.main-section2 { /* estiliza tudo que está dentro da segunda section: botoes, formulario e info da conta*/
   display: flex;
   justify-content: space-around;
   flex-direction: column;
   flex-wrap: wrap-reverse;
-  position: relative; left: 10px; top: -520px;
-  align-items: right;
+  align-items: flex-start;
 }
 
-.footer{ /* estiliza o rodapé da página, tudo em amarelo*/
-  background: white;
-  justify-content: space-around;
+footer {
+  text-align: center;
+  padding: 3px;
+  background-color: #6D3BFF;
+  color: white;
+  position: relative; bottom: 0px;
+  font-family: inconsolata;
 }
 .footer-text{ /* estiliza só o all rights deserved */
     display: flex;
@@ -155,5 +159,8 @@ body { /* pagina inteira */
     justify-content: space-between;
 }
 </style>
-  
+
+<footer>
+  <p>Be-Ut.com</p>
+</footer>
 
